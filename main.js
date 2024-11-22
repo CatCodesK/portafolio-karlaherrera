47ea621846a96283d4f3647a6eb6f650981f614f
Karla Herrera <karla2k13@gmail.com>
2024-11-22 01:28:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+//Comentarios randoms
+typewriter.typeString('Ing. en Redes Digitales y Ciberseguridad')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Aqui iría algo super innovador... ¡claro!, si lo hubiera pensado')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>PROGRAMADO</strong>')
+    .pauseFor(2500)
+    .start();
